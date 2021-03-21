69d6e52ac187bc9a4cab5567bfde146443414f3c
Gonzalo Cercos Vazquez <gcercos@altech.es>
2021-03-21 13:17:55 +0100

    Feat: Adding a success path

--- src/BookList.js
+++ src/BookList.js
@@ -11,8 +11,8 @@ const BookList = ({loading, error, books}) => {
 
     return <div data-test='book-list'>
         {
-            books.map(book => (<div className='book-item'>
-            <h2 className='title'>{book.name}</h2>
+            books.map(book => (<div className='book-item' key={book.id}>
+                <h2 className='title'>{book.name}</h2>
             </div>))
         }
     </div>;
