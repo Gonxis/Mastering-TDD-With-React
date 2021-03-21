6c05f5d2918853638567bfb624f02d07f246d161
Gonzalo Cercos Vazquez <gcercos@altech.es>
2021-03-21 09:10:33 +0100

    Fixture: Set data and delete it before and after each test

--- cypress/integration/ui/bookish.spec.js
+++ cypress/integration/ui/bookish.spec.js
@@ -1,6 +1,33 @@
 import { ExpansionPanelActions } from "@material-ui/core";
+import axios from "axios";
 
 describe('Bookish application', () => {
+    before(() => {
+        return axios
+            .delete('http://localhost:8080/books?_cleanup=true')
+            .catch(err => err);
+    });
+
+    afterEach(() => {
+        return axios
+            .delete('http://localhost:8080/books?_cleanup=true')
+            .catch(err => err)
+    })
+
+    beforeEach(() => {
+        const books = [
+            { 'name': 'Refactoring', 'id': 1 },
+            { 'noame': 'Domain-driven design', 'id': 2 }
+            { 'noame': 'Building Microservices', 'id': 3 }
+        ]
+
+        return books.map(item => 
+            axios.post('http://localhost:8080/books', item,
+                { headers: { 'Content-Type': 'application/json' } }
+            )
+        )
+    })
+
     it('Visits the bookish', () => {
         cy.visit('http://localhost:3000/');
         cy.get('h2[data-test="heading"]').contains('Bookish')
@@ -10,10 +37,10 @@ describe('Bookish application', () => {
         cy.visit('http://localhost:3000/');
         cy.get('div[data-test="book-list"]').should('exist');
         cy.get('div.book-item').should(books => {
-            expect(books).to.have.length(2);
+            expect(books).to.have.length(3);
 
             const titles = [...books].map(x => x.querySelector('h2').innerHTML);
-            expect(titles).to.deep.equal(['Refactoring', 'Domain-driven design'])
+            expect(titles).to.deep.equal(['Refactoring', 'Domain-driven design', 'Building Microservices'])
         })
     })
 })
