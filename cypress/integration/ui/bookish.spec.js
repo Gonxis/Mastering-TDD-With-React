1b6f70dced6ab0131f7d3d1fb14cccad134f5b9d
Gonzalo Cercos <goncholo@gmail.com>
2021-04-09 20:32:57 +0200

    Refactor: Extracting Cypress repetitive actions into functions.

--- cypress/integration/ui/bookish.spec.js
+++ cypress/integration/ui/bookish.spec.js
@@ -1,35 +1,40 @@
-import { ExpansionPanelActions } from "@material-ui/core";
-import axios from "axios";
+import {
+    checkAppTitle,
+    checkBookDetail,
+    checkBookListWith,
+    cleanUpStubBooks,
+    feedStubBooks,
+    gotoApp,
+    gotoNthBookInTheList,
+    performSearch
+} from "../../helpers";
 
 describe('Bookish application', () => {
+    beforeEach(() => {
+        feedStubBooks();
+        gotoApp();
+    });
+
+    afterEach(() => {
+        cleanUpStubBooks();
+    });
+
     it('Visits the bookish', () => {
-        cy.visit('http://localhost:3000/');
-        cy.get('h2[data-test="heading"]').contains('Bookish')
+        checkAppTitle();
     })
 
     it('Shows a book list', () => {
-        cy.visit('http://localhost:3000/');
-        cy.get('div[data-test="book-list"]').should('exist');
-        cy.get('div.book-item').should(books => {
-            expect(books).to.have.length(4);
-
-            const titles = [...books].map(x => x.querySelector('h2').innerHTML);
-            expect(titles).to.deep.equal(['Refactoring', 'Domain-driven design', 'Building Microservices', 'Acceptance Test Driven Development with React'])
-        })
+        checkBookListWith(['Refactoring', 'Domain-driven design', 'Building Microservices', 'Acceptance Test Driven Development with React']);
     })
 
     it('Goes to the detail page', () => {
-        cy.visit('http://localhost:3000');
-        cy.get('div.book-item').contains('View Details').eq(0).click();
-        cy.url().should('include', 'books/1');
-        cy.get('h2.book-title').contains('Refactoring');
+        gotoNthBookInTheList(0);
+        checkBookDetail();
     })
 
-    it('Searches for a title', () => {
-        cy.visit('http://localhost:3000/');
-        cy.get('div.book-item').should('have.length', 4);
-        cy.get('[data-test="search"] input').type('design');
-        cy.get('div.book-item').should('have.length', 1);
-        cy.get('div.book-item').eq(0).contains('Domain-driven design');
+    it('Search for a title', () => {
+        checkBookListWith(['Refactoring', 'Domain-driven design', 'Building Microservices', 'Acceptance Test Driven Development with React']);
+        performSearch('design');
+        checkBookListWith(['Domain-driven design']);
     });
 })
