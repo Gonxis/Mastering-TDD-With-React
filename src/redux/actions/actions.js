ad6e1c66653bb27aec6b0ae621965db21f808191
Gonzalo Cercos <goncholo@gmail.com>
2021-04-10 13:49:53 +0200

    Refactor: FetchBooks action to use state

--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -5,13 +5,14 @@ export const setSearchTerm = (term) => {
     return { type: types.SET_SEARCH_TERM, term }
 }
 
-export const fetchBooks = (term) => {
-    return dispatch => {
-        dispatch({type: types.FETCH_BOOKS_PENDING})
-        return axios.get(`http://localhost:8080/books?q=${term || ''}`).then(res => {
-            dispatch({type: types.FETCH_BOOKS_SUCCESS, books: res.data})
+export const fetchBooks = () => {
+    return (dispatch, getState) => {
+        dispatch({ type: types.FETCH_BOOKS_PENDING });
+        const state = getState();
+        return axios.get(`http://localhost:8080/books?q=${state.term || ''}`).then(res => {
+            dispatch({ type: types.FETCH_BOOKS_SUCCESS, books: res.data })
         }).catch(err => {
-            dispatch({type: types.FETCH_BOOKS_FAILED, err: err.message})
+            dispatch({ type: types.FETCH_BOOKS_FAILED, err: err.message })
         })
     }
 }
