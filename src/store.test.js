b946dc7bea59abe7c28e992a6553b68b2deae99d
Gonzalo Cercos <goncholo@gmail.com>
2021-04-10 14:22:20 +0200

    Feat: Adding an integration test in store

--- src/store.test.js
+++ src/store.test.js
@@ -19,4 +19,19 @@ describe('Store', () => {
             expect(state.books).toEqual(books);
         });
     });
+
+    it('Perform a search', () => {
+        axios.get = jest
+            .fn()
+            .mockImplementation(() => Promise.resolve({data: books}));
+
+        store.dispatch(actions.setSearchTerm('domain'))
+
+        return store.dispatch(actions.fetchBooks()).then(() => {
+            const state = store.getState();
+
+            expect(state.term).toEqual('domain');
+            expect(axios.get).toHaveBeenCalledWith('http://localhost:8080/books?q=domain');
+        });
+    });
 });
