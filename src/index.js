fca659223d47da51c2b0b452fbf4b72803ed0eee
Gonzalo Cercos <goncholo@gmail.com>
2021-04-10 15:29:53 +0200

    Feat: Adding the store to the app and failed case

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<Router>
-  <App />
-</Router>,
-  document.getElementById('root')
-);
+import { Provider } from 'react-redux';
+import store from './store';
+
+const root = <Provider store={store}>
+  <Router>
+    <App />
+  </Router>
+</Provider>
+
+ReactDOM.render(root, document.getElementById('root'));
