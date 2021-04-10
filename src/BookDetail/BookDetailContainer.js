42eb915bda916729ff6dccf5438c08be734ba2bd
Gonzalo Cercos <goncholo@gmail.com>
2021-04-10 13:09:01 +0200

    ...

--- src/BookDetail/BookDetailContainer.js
+++ src/BookDetail/BookDetailContainer.js
@@ -3,8 +3,8 @@ import {useRemoteService} from '../hooks';
 import BookDetail from './BookDetail';
 
 const BookDetailContainer = ({match}) => {
+    //const book = fetchBookById(match.params.id);
     const { data } = useRemoteService(`http://localhost:8080/books/${match.params.id}`, {});
-
     return (<BookDetail book={data} />);
 }
 
