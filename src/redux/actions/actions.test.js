97d8d127ebf3053016d01f44bd23726fca04d73d
Gonzalo Cercos <goncholo@gmail.com>
2021-04-10 12:23:38 +0200

    Feat: Adding the implementation of FetchBooks test

--- src/redux/actions/actions.test.js
+++ src/redux/actions/actions.test.js
@@ -1,4 +1,4 @@
-import { setSearchTerm } from './actions';
+import { setSearchTerm, fetchBooks } from './actions';
 
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
